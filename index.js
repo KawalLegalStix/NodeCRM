1fbdcfac8e3f60ac7be6fe642a0eb04a4d38478b
KawalLegalstix <kawaljeet.mehra@legalstix.com>
2024-01-09 11:31:25 +0530

    Toml added

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
   res.send('Node.js Setup Success!')
 });
 
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 }); 
